ee42860fc4935ee7e84d7f1774beedf15ed34189
Anukul Pandey <anukul.030601@gmail.com>
2023-07-24 21:56:06 +0530

    feat: refactored

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import { useState ,useEffect} from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 import { Web3Auth } from "@web3auth/modal";
 import { Keyring } from "@polkadot/api";
 import { CHAIN_NAMESPACES } from "@web3auth/base";
-import {u8aToHex} from "@polkadot/util";
-import {wrapBytes} from '@reef-defi/extension-dapp';
-import { decodeAddress, signatureVerify } from '@reef-defi/util-crypto';
-import {getProvider} from './utils';
-import {utils,BigNumber} from "ethers";
-import Uik from '@reef-defi/ui-kit'
+import { u8aToHex } from "@polkadot/util";
+import { wrapBytes } from "@reef-defi/extension-dapp";
+import { decodeAddress, signatureVerify } from "@reef-defi/util-crypto";
+import { getProvider } from "./utils";
+import { utils, BigNumber } from "ethers";
+import Uik from "@reef-defi/ui-kit";
 
-const clientId = "BJJcvvvZaGzrWK90JRN2dSQ3g67rMGIn6hh9sWDIg7SVvo6se_1JD1k8_86VshiIu1dllrcj5Pr3wYDO10lFoB0";
-
+const clientId =
+  "BJJcvvvZaGzrWK90JRN2dSQ3g67rMGIn6hh9sWDIg7SVvo6se_1JD1k8_86VshiIu1dllrcj5Pr3wYDO10lFoB0";
 
 function App() {
   const [web3auth, setWeb3auth] = useState(null);
+  const [PrivateKey, setPrivateKey] = useState(null);
+  const [user,setUser] = useState(null);
+  const [isAccountModalOpen, setIsAccountModalOpen] = useState(false)
 
   useEffect(() => {
     const init = async () => {
@@ -29,14 +32,15 @@ function App() {
           },
         });
 
-
         setWeb3auth(web3auth);
 
         await web3auth.initModal();
-        const web3authProvider = web3auth.connect();
-        console.log(web3authProvider)
-
-        console.log("initialized")
+        web3auth.connect();
+        const privateKey = await web3auth.provider.request({
+          method: "private_key",
+        });
+        setPrivateKey(privateKey);
+        
       } catch (error) {
         console.error(error);
       }
@@ -45,94 +49,125 @@ function App() {
     init();
   }, []);
 
+  useEffect(() => {
+    getUserInfo();
+  }, [PrivateKey!=null])
+
+  useEffect(() => {
+    getNativeAddress();
+  }, [user!=null])
+  
+
   const getUserInfo = async () => {
-    if (web3auth.status!=="connected") {
-      console.log("web3auth not initialized yet");
-      alert("Web3 auth not initialized , you need to login first")
-      return;
-    }
     const user = await web3auth.getUserInfo();
-    console.log(user);
-    alert(`
-    name : ${user.name}
-    email : ${user.email} 
-    `)
+    setUser(user);
   };
 
-  const getNativeAddress = async ()=>{
-    const privateKey = await web3auth.provider.request({ method: "private_key" })
-const keyring = new Keyring({ ss58Format: 42,type: "sr25519" });
-const _keyPair = keyring.addFromUri("0x" + String(privateKey));
-const account = _keyPair?.address;
-alert(account)
-  }
+  const getNativeAddress = async () => {    
+    const keyring = new Keyring({ ss58Format: 42, type: "sr25519" });
+    const _keyPair = keyring.addFromUri("0x" + String(PrivateKey));
+    let userData = user; 
+    if(user!=null){
+      userData['address'] = _keyPair?.address;
+      setUser(userData);
+      console.log(userData);
+    }
+  };
 
   const isValidSignature = (signedMessage, signature, address) => {
     const publicKey = decodeAddress(address);
     const hexPublicKey = u8aToHex(publicKey);
-  
+
     return signatureVerify(signedMessage, signature, hexPublicKey).isValid;
   };
 
-  const signRaw = async(message)=>{
-    const privateKey = await web3auth.provider.request({ method: "private_key" })
-    const keyring = new Keyring({ ss58Format: 42,type: "sr25519" });
+  const signRaw = async (message) => {
+    const privateKey = await web3auth.provider.request({
+      method: "private_key",
+    });
+    const keyring = new Keyring({ ss58Format: 42, type: "sr25519" });
     const _keyPair = keyring.addFromUri("0x" + String(privateKey));
-   const signature= u8aToHex(_keyPair.sign(wrapBytes(message)))
-   const _isValid = await isValidSignature(message,signature,_keyPair.address)
-   alert(`signature: ${signature}\nverified: ${_isValid}`);
+    const signature = u8aToHex(_keyPair.sign(wrapBytes(message)));
+    const _isValid = await isValidSignature(
+      message,
+      signature,
+      _keyPair.address
+    );
+    alert(`signature: ${signature}\nverified: ${_isValid}`);
 
-   return signature;
-  }
+    return signature;
+  };
 
-  const makeTransaction = async()=>{
-    const privateKey = await web3auth.provider.request({ method: "private_key" })
-    const keyring = new Keyring({ ss58Format: 42,type: "sr25519" });
+  const makeTransaction = async () => {
+    const privateKey = await web3auth.provider.request({
+      method: "private_key",
+    });
+    const keyring = new Keyring({ ss58Format: 42, type: "sr25519" });
     const _keyPair = keyring.addFromUri("0x" + String(privateKey));
     const provider = await getProvider();
     const txHash = await provider.tx.balances
       .transfer("5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP", 12345)
       .signAndSend(_keyPair);
-console.log(txHash.toHuman());
-  }
+    console.log(txHash.toHuman());
+  };
 
-  const getBalance = async()=>{
-    const privateKey = await web3auth.provider.request({ method: "private_key" })
-    const keyring = new Keyring({ ss58Format: 42,type: "sr25519" });
+  const getBalance = async () => {
+    const privateKey = await web3auth.provider.request({
+      method: "private_key",
+    });
+    const keyring = new Keyring({ ss58Format: 42, type: "sr25519" });
     const _keyPair = keyring.addFromUri("0x" + String(privateKey));
     const provider = await getProvider();
     const data = await provider.query.system.account(_keyPair.address);
-    alert(utils.formatUnits( BigNumber.from(data.data.free.toString())._hex,18)+'REEF');
-  }
-
+    alert(
+      utils.formatUnits(BigNumber.from(data.data.free.toString())._hex, 18) +
+        "REEF"
+    );
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-      <Uik.ReefLogo />
-    <br />
-        <button onClick={getUserInfo} className="card">
-            Get User Info
-          </button>
-          <br />
-        <button onClick={getNativeAddress} className="card">
-            Get Native Address
-          </button>
-          <br/>
-        <button onClick={()=>signRaw("hello anukul")} className="card">
-            Sign Raw
-          </button>
-          <br/>
+        <Uik.ReefLogo />
+        {user!=null?
+        <div>
+          <Uik.Button text={user.name} rounded fill onClick={()=>setIsAccountModalOpen(true)} className="usernameBtn" size='large'/>
+        <button onClick={() => signRaw("hello anukul")} className="card">
+          Sign Raw
+        </button>
+        <br />
         <button onClick={makeTransaction} className="card">
-            Make transaction
-          </button>
-          <br/>
+          Make transaction
+        </button>
+        <br />
         <button onClick={getBalance} className="card">
-            Get Balance
-          </button>
-          <br/>
-
-     
+          Get Balance
+        </button>
+        <Uik.Modal
+    title='Account Info'
+    isOpen={isAccountModalOpen}
+    onClose={() => setIsAccountModalOpen(false)}
+    onOpened={() => {}}
+    onClosed={() => {}}
+    footer={
+      <>
+        <Uik.Button text='Close' onClick={() => setIsAccountModalOpen(false)}/>
+      </>
+    }
+  >
+    <div>
+    <Uik.Avatar
+    name={user.name}
+    image={user.profileImage}
+    size="large"
+    className="accountInfoContent"
+  />
+    <Uik.Text className="accountInfoContent">Email: {user.email} </Uik.Text>
+    <Uik.Text className="accountInfoContent">Address: {user.address} </Uik.Text>
+    </div>
+  </Uik.Modal>
+        </div>
+        :<div>You need to login to see this page</div>}
       </header>
     </div>
   );
